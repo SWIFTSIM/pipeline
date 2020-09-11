f15f70a4e8ee1a8c4b428bb9612bdc4263e30ae8
Josh Borrow <joshua.borrow@durham.ac.uk>
2020-09-11 20:25:57 +0100

    Added packaging

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+from swiftpipeline import __version__
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="swiftpipeline",
+    version=__version__,
+    description="Pipeline for producing galaxy scaling relation plots.",
+    url="https://joshborrow.com",
+    author="Josh Borrow",
+    author_email="josh@joshborrow.com",
+    packages=setuptools.find_packages(),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[],
+    scripts=[],
+)
